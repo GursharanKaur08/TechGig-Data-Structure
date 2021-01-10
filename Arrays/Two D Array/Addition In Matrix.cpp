14e8286137021bad5750654261caa135acc270b4
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-10 23:37:32 +0530

    Update Addition In Matrix.cpp

--- Arrays/Two D Array/Addition In Matrix.cpp
+++ Arrays/Two D Array/Addition In Matrix.cpp
@@ -1,3 +1,15 @@
+/*Addition In Matrix 
+Given a 2D array, Sum the elements of the array.
+
+Input Format
+You will taking two integers as input on one line separated by space representing rows and columns of the matrix. Following lines after that will be elements of the matrix with each element separated by space.
+
+Constraints
+1 <= n,m <= 1000
+
+Output Format
+Print the sum to the STDOUT.*/
+
 #include <iostream>
 using namespace std;
 int main()
