940c923d9c7f8d075c0b147728ff587dba36ff46
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-10 23:43:21 +0530

    Update Print Even.cpp

--- Arrays/Two D Array/Print Even.cpp
+++ Arrays/Two D Array/Print Even.cpp
@@ -1,3 +1,14 @@
+/*Print Even
+Given a 2D array, print the even numbers of it.
+
+Input Format
+You will taking two integers as input on one line separated by space representing rows and columns of the matrix. Following lines after that will be elements of the matrix with each element separated by space.
+
+Constraints
+1 <= n,m <= 1000
+
+Output Format
+Print the even elements of the matrix with each element separated by space.*/
 
 #include <iostream>
 using namespace std;
