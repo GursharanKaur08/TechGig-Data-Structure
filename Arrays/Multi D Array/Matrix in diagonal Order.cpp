7a153f9cf2139de298887d2de33be35fc86ac860
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-11 23:14:20 +0530

    Update Matrix in diagonal Order.cpp

--- Arrays/Multi D Array/Matrix in diagonal Order.cpp
+++ Arrays/Multi D Array/Matrix in diagonal Order.cpp
@@ -1,3 +1,16 @@
+/*2D Array - Matrix in diagonal Order
+Given a 2D matrix, print all elements of the given matrix in diagonal order. 
+
+Input Format
+On first line you need to tell that how many rows and columns your matrix need to have and these values should be separated by space.
+Then after that, each line will represent will represent each row and you need to enter numbers which each rows should have separated by a space.
+
+Constraints
+1 <= n,m <= 1000
+
+Output Format
+Print the resultant matrix in the diagonal wave from as specified to the stdout.*/
+
 #include <iostream>
 using namespace std;
 int main()
